8d688f348fa816aa8a9175a0e7652f706ece27ad
Rémy HUBSCHER <hubscher.remy@gmail.com>
2021-04-16 10:58:47 +0200

    Add screen display.

--- src/menu/views.py
+++ src/menu/views.py
@@ -4,11 +4,12 @@ from .models import Screen
 
 
 def screen_viewer(request, screen_id):
-    screen = Screen.objects.get(screen_id)
-    context = {'screen': screen}
-    return render(request, 'menu/screen_viewer.html', context)
+    screen = Screen.objects.get(pk=screen_id)
+    context = {"screen": screen}
+    return render(request, "menu/screen_viewer.html", context)
+
 
 def index(request):
     screens = Screen.objects.all()
-    context = {'screens': screens}
-    return render(request, 'menu/index.html', context)
+    context = {"screens": screens}
+    return render(request, "menu/index.html", context)
