7258dfe5a97016f509e65db96b9c713725c63fbd
Rémy HUBSCHER <hubscher.remy@gmail.com>
2021-04-28 19:03:32 +0200

    Order items with pages and categories.

--- src/menu/admin.py
+++ src/menu/admin.py
@@ -1,15 +1,41 @@
 from django.contrib import admin
 
+from admin_ordering.admin import OrderableAdmin
+
 from .models import Category, Product, Screen
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "category")
+class ProductAdmin(OrderableAdmin, admin.ModelAdmin):
+    ordering = ("category__ordering", "ordering")
+
+    list_display = ("name", "category", "ordering")
     order_by = ("category__pk", "name")
     list_filter = ["category"]
     search_fields = ["name", "category__name"]
+    list_editable = ["ordering"]
 
 
-admin.site.register(Screen)
-admin.site.register(Category)
+@admin.register(Screen)
+class ScreenAdmin(OrderableAdmin, admin.ModelAdmin):
+    ordering = ("ordering",)
+
+    # You may optionally hide the ordering field in the changelist:
+    # ordering_field_hide_input = False
+
+    # The ordering field must be included both in list_display and
+    # list_editable:
+    list_display = ["name", "ordering"]
+    list_editable = ["ordering"]
+
+@admin.register(Category)
+class CategoryAdmin(OrderableAdmin, admin.ModelAdmin):
+    ordering = ("ordering",)
+
+    # You may optionally hide the ordering field in the changelist:
+    # ordering_field_hide_input = False
+
+    # The ordering field must be included both in list_display and
+    # list_editable:
+    list_display = ["name", "ordering"]
+    list_editable = ["ordering"]
--- src/menu/models.py
+++ src/menu/models.py
@@ -3,6 +3,7 @@ from os.path import splitext
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.text import slugify
+from admin_ordering.models import OrderableModel
 
 
 def category_upload_to(instance, filename):
@@ -12,18 +13,19 @@ def category_upload_to(instance, filename):
     return f"uploads/category_{slugify(instance.name)}{ext}"
 
 
-class Category(models.Model):
+class Category(OrderableModel):
     name = models.CharField(max_length=75)
     illustration = models.ImageField(upload_to=category_upload_to, blank=True, null=True)
 
     class Meta:
+        ordering = ["ordering"]
         verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
 
 
-class Product(models.Model):
+class Product(OrderableModel):
     name = models.CharField(max_length=75)
     priceEuroCents = models.IntegerField(
         validators=[MaxValueValidator(9999), MinValueValidator(1)],
@@ -37,11 +39,17 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ["ordering"]
 
-class Screen(models.Model):
+
+class Screen(OrderableModel):
     name = models.CharField(max_length=75)
     title = models.CharField(max_length=150)
     categories = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ["ordering"]
